6e5623487af08fbd45fab0fc397fb6bd616bc8ac
Canyon Mathis <canyon.mathis@gmail.com>
2020-01-31 19:07:44 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -8,22 +8,6 @@ def my_each(array)
   end
 end
 
-my_each(list) {|i| p " #{i} "}
-
-
-def hello(array)
-  i = 0
-  collection = []
-  while i < array.length
-    collection << yield(array[i])
-    i += 1
-  end
-  collection
-end
-
-hello(list) {|i| p "#{i} "}
-
-my_each(list) {|i| p " #{i} "}
 
 
 
